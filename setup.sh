30d44ceade69ff0c71efb6e633c807b9ddfae602
pen <iamapen@mail.studiopoppy.com>
2013-11-19 05:26:54 +0900

    fixed exclude

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 BASE_DIR=$(cd $(dirname $0);pwd)
 FILES=$(find . -mindepth 1 -maxdepth 1 -exec basename {} \;)
-FILES=$(echo $FILES | sed -e "s/ setup.sh//")
-FILES=$(echo $FILES | sed -e "s/ setup_cygwin.sh//")
-FILES=$(echo $FILES | sed -e "s/ [a-zA-Z0-9._]\+_cygwin//g")
-FILES=$(echo $FILES | sed -e "s/ [a-zA-Z0-9._]\+\.swp//g")
+FILES=$(echo $FILES | sed -e "s/setup.sh \?//")
+FILES=$(echo $FILES | sed -e "s/setup_cygwin.sh \?//")
+FILES=$(echo $FILES | sed -e "s/[a-zA-Z0-9._]\+_cygwin \?//g")
+FILES=$(echo $FILES | sed -e "s/[a-zA-Z0-9._]\+\.swp \?//g")
 
 for FILE in $FILES
 do
+  echo -e
 	echo link "$HOME/$FILE -> $BASE_DIR/$FILE"
-	ln -sfT "$BASE_DIR/$FILE" "$HOME/$FILE"
+	ln -siT "$BASE_DIR/$FILE" "$HOME/$FILE"
 done
