7e08212bf9bb8bf58da020d0cae7a7327a02bdd9
pen <iamapen@mail.studiopoppy.com>
2013-11-21 14:36:25 +0900

    分割.vimrc

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 BASE_DIR=$(cd $(dirname $0);pwd)
-FILES=$(find . -mindepth 1 -maxdepth 1 -exec basename {} \;)
+
+# dotfiles
+FILES=$(find . -mindepth 1 -maxdepth 1 -type f -exec basename {} \;)
 FILES=$(echo $FILES | sed -e "s/setup.sh \?//")
 FILES=$(echo $FILES | sed -e "s/setup_cygwin.sh \?//")
 FILES=$(echo $FILES | sed -e "s/[a-zA-Z0-9._]\+_cygwin \?//g")
@@ -10,6 +12,16 @@ FILES=$(echo $FILES | sed -e "s/[a-zA-Z0-9._]\+\.swp \?//g")
 for FILE in $FILES
 do
   echo -e
-	echo link "$HOME/$FILE -> $BASE_DIR/$FILE"
-	ln -siT "$BASE_DIR/$FILE" "$HOME/$FILE"
+  echo link "$HOME/$FILE -> $BASE_DIR/$FILE"
+  ln -siT "$BASE_DIR/$FILE" "$HOME/$FILE"
 done
+
+# vimrc.d
+echo -e
+echo link "$HOME/.vimrc.d -> $BASE_DIR/.vimrc.d"
+ln -siT "$BASE_DIR/.vimrc.d" "$HOME/.vimrc.d"
+
+# bash-completion.d
+echo -e
+echo link "$HOME/etc/bash_completion.d -> $BASE_DIR/etc/bash_completion.d"
+ln -siT "$BASE_DIR/etc/bash_completion.d" "$HOME/etc/bash_completion.d"
--- setup_cygwin.sh
+++ setup_cygwin.sh
@@ -4,7 +4,7 @@ BASE_DIR=$(cd $(dirname $0);pwd)
 
 source setup.sh
 
-FILES=$(find . -mindepth 1 -maxdepth 1 -name *_cygwin -exec basename {} \;)
+FILES=$(find . -mindepth 1 -maxdepth 1 -type f -name *_cygwin -exec basename {} \;)
 FILES=$(echo $FILES | sed -e "s/setup.sh \?//")
 FILES=$(echo $FILES | sed -e "s/setup_cygwin.sh \?//")
 FILES=$(echo $FILES | sed -e "s/[a-zA-Z0-9._]\+\.swp \?//g")
@@ -14,6 +14,6 @@ for FILE in $FILES
 do
   LINKNAME=$(echo $FILE | sed -e "s/_cygwin$//")
   echo -e
- 	echo link "$HOME/$LINKNAME-> $BASE_DIR/$FILE"
- 	ln -siT "$BASE_DIR/$FILE" "$HOME/$LINKNAME"
+  echo link "$HOME/$LINKNAME-> $BASE_DIR/$FILE"
+  ln -siT "$BASE_DIR/$FILE" "$HOME/$LINKNAME"
 done
